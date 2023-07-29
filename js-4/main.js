72204b54d83e96eb09ff47f0afbf703f6efe221b
000.goz <116011109+Samandar0070@users.noreply.github.com>
2023-07-29 13:13:42 +0500

    Add files via upload

---
+++ js-4/main.js
@@ -0,0 +1,13 @@
+function showPrimes(n) {
+    for (let i = 2; i < n; i++) {
+        if(!isPrime(i)) continue;
+        alert(i);
+    }
+}
+function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+        if(n % i == 0) return false;
+    }
+    return true;
+}
+showPrimes(prompt("berilgan songacha tub so'nlarni aniqlab beradi"))
